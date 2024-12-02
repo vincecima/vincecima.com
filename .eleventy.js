d6c4ecb3a32768c627f81a5aa387ee1aa0c0e596
Vince Cima <contact@vincecima.com>
2024-12-01 23:21:18 -0600

    Expose feeds for future posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,36 @@
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+
 export default function (eleventyConfig) {
     eleventyConfig.setInputDirectory("site");
     eleventyConfig.setOutputDirectory("public")
     eleventyConfig.addBundle("css");
+
+    eleventyConfig.addPlugin(feedPlugin, {
+        type: "json",
+        outputPath: "feed.json",
+        collection: {
+            name: "posts"
+        },
+        metadata: {
+            base: "https://vincecima.com",
+            language: "en",
+            title: "vincecima.com",
+            author: {
+                name: "Vince Cima",
+            }
+        }
+    });
+    eleventyConfig.addPlugin(feedPlugin, {
+        collection: {
+            name: "posts"
+        },
+        metadata: {
+            base: "https://vincecima.com",
+            language: "en",
+            title: "vincecima.com",
+            author: {
+                name: "Vince Cima",
+            }
+        }
+    });
 };
